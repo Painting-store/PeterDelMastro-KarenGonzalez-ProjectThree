344d0ed6ebfdee800b38d81b078c8bf8578d5cb0
Karen Gonzalez <57917529+kvaleriagonzalez@users.noreply.github.com>
2021-04-12 08:38:16 -0400

    add appjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import './Setup.css';
 import firebase from './firebase';
 import {useEffect,useState} from 'react'
 import './App.css';
 
 function App() {
   const [paintings, setPaintings] = useState([]);
-  const cartArray = [];
   
   useEffect(() => {
     //this is our variable to hold our reference to our database
@@ -28,52 +28,46 @@ function App() {
     })
   }, [])
 
-
-
   const addToCart = function (painting) {
-    //create an array to hold the objects
-
-    cartArray.push(painting);
-
-
-    console.log('clicked')
-    console.log(cartArray);
-
-
- }
+    const allPaint = [];
+    for(const paint in painting) {
+    allPaint.push(painting[paint]);
+    }
+    console.log(allPaint);
+    
+  }
 
   return (
-    <div className="App">
-     <h1>store</h1>
-      <i className="fas fa-shopping-cart"></i>
-      <ul className= "paintingsGallery">
-          {paintings.map((painting) => {
-          // console.log(painting)
-            return (
-              <div class= "paintingCell">
-                <li key={painting.key}>
-                  <div class = "imageContainer">
+    <div className="App wrapper">
+      <div className ="flex">
+        <h1>store</h1>
+      </div>
+      <div className="flex-icon">
+          <i className="fas fa-shopping-cart"></i>
+       </div>
+        <div className="flexContainer">
+          <ul className= "paintingsGallery grid-container">
+            {paintings.map((painting) => {
+                return(
+                  <li key={painting.key}>
                     <img src={painting.name.url} alt={painting.name.description}></img>
-                    </div>
-              <p>{painting.name.title}</p>
-                  <p>{painting.name.description}</p>
-                  <p>{painting.name.price}</p>
-              <button onClick={ () => {
-              addToCart(painting)}
-              }>
-              Add to Cart
-            </button>
-                </li>
-                </div>
-          )
-        })}
-        </ul>
-      
-    </div>
-     
-    
+                    <p className="space">{painting.name.title}</p>
+                    <p className="Pspace">{painting.name.description}</p>
+                    <p className="Pspace price">{painting.name.price}</p>
+                    <button  className="space" onClick={ () => {addToCart(painting)
+                    }
+                  }>Add to Cart</button>
+                  </li>
+                )
+            })}
+          </ul>
+        </div>
+        <div>
 
+        </div>
+    </div>
   );
 }
 
 export default App;
+
