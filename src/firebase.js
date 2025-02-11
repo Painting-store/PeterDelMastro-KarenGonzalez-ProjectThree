e53b697b9bfa24dc619cdc01e5c8643132d45ff1
peterdelm <peterdelmastro@hotmail.com>
2025-02-10 23:37:56 -0500

    new db

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,25 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
- 
- // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAXQz1-CBFMt57j5yQCav8oEOtHdWaPcpE",
-    authDomain: "art-store-6d140.firebaseapp.com",
-    projectId: "art-store-6d140",
-    storageBucket: "art-store-6d140.appspot.com",
-    messagingSenderId: "690801099215",
-    appId: "1:690801099215:web:4e5952ffcbacfd9a3852ba"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+import firebase from "firebase/app";
+import "firebase/database";
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8tjhjKpLp3F78VYXjyQ3FZd2pKaU0x3U",
+
+  authDomain: "art-store2.firebaseapp.com",
+
+  databaseURL: "https://art-store2-default-rtdb.firebaseio.com",
+
+  projectId: "art-store2",
+
+  storageBucket: "art-store2.firebasestorage.app",
+
+  messagingSenderId: "441899092930",
+
+  appId: "1:441899092930:web:753051962e8bbc9e77245b",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
