7e31b8bcd4ef5d6c4750a52ef5ed08cfd93902cc
peterdelm <peterdelmastro@hotmail.com>
2025-02-11 23:11:58 -0500

    fixed addition on checkout popup

--- src/App.js
+++ src/App.js
@@ -1,105 +1,145 @@
-import './Setup.css';
-import firebase from './firebase';
-import {useEffect,useState} from 'react'
-import './App.css';
-import CheckOutPopup from './Popup.js';
-import imageSrc from './images/p-paint.png';
+import "./Setup.css";
+import firebase from "./firebase";
+import { useEffect, useState } from "react";
+import "./App.css";
+import CheckOutPopup from "./Popup.js";
+import imageSrc from "./images/p-paint.png";
 
 function App() {
-    const [paintings, setPaintings] = useState([]);
-    const [allPaint, setallPaint] = useState([]);
-    const [cartClick, setcartClick] = useState(false);
-    const [totalPrice, setTotalPrice] = useState(0);
-  
+  const [paintings, setPaintings] = useState([]);
+  const [allPaint, setallPaint] = useState([]);
+  const [cartClick, setcartClick] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+
   //useEffect to fetch our data from the firebase
   useEffect(() => {
     const dataBaseRef = firebase.database().ref();
-    dataBaseRef.on('value', response => {
-      
-    const newState = [];
-    const returnedDatafromFirebase = response.val();
+    dataBaseRef.on("value", (response) => {
+      const newState = [];
+      const returnedDatafromFirebase = response.val();
 
       for (let key in returnedDatafromFirebase) {
         newState.push({
-          key:key,
-          name: returnedDatafromFirebase[key]
-        })
+          key: key,
+          name: returnedDatafromFirebase[key],
+        });
       }
       setPaintings(newState);
-    })
-  }, [])
-
+    });
+  }, []);
 
   // function to add paintings to the cart and tally the total
   const addToCart = function (painting) {
     const newPaint = [...allPaint];
-    const newTotal = totalPrice;
+    const rawTotal = totalPrice?.toString().replace(/,/g, "");
+    const oldTotal = parseFloat(rawTotal);
+    const rawPrice = painting.name.price?.toString().replace(/,/g, "");
+    const newPrice = parseFloat(rawPrice) || 0;
     painting.name.value = false;
     newPaint.push(painting.name);
     setallPaint(newPaint);
-    setTotalPrice(newTotal + painting.name.price);
-  }
+    const value = oldTotal + newPrice;
+    setTotalPrice(value);
+  };
 
   const removePainting = function (cartPainting, deletePaint) {
-    const deleteP = cartPainting.findIndex((object) => object.title === deletePaint.title)
+    const rawTotal = totalPrice?.toString().replace(/,/g, "");
+    const oldTotal = parseFloat(rawTotal);
+    const rawPrice = deletePaint.price?.toString().replace(/,/g, "");
+    const newPrice = parseFloat(rawPrice) || 0;
+    const deleteP = cartPainting.findIndex(
+      (object) => object.title === deletePaint.title
+    );
     const newArray = [...allPaint];
-    newArray.splice(deleteP,1);
-    setTotalPrice(totalPrice - deletePaint.price);
+    newArray.splice(deleteP, 1);
+    const value = oldTotal - newPrice;
+    setTotalPrice(value);
     setallPaint(newArray);
-    console.log(totalPrice);
-  }
+  };
 
   //function to toggle the checkout boolean
   const checkOut = function () {
     setcartClick(true);
-    
+
     if (cartClick === true) {
       setcartClick(false);
-    } 
-  }
+    }
+  };
 
   return (
-   <div className="App wrapper">
-        {cartClick === true
-          ? <CheckOutPopup arrayOfPaintings={allPaint}  totalCost={totalPrice} remove={removePainting}
-          />
-        : null}
+    <div className="App wrapper">
+      {cartClick === true ? (
+        <CheckOutPopup
+          arrayOfPaintings={allPaint}
+          totalCost={totalPrice}
+          remove={removePainting}
+        />
+      ) : null}
 
-        <div className="flex">
-          <h1>Del Mastro & Gonzalez Gallery <i className="fas fa-shopping-cart" id="cart"  onClick={() => { checkOut(allPaint) }}></i></h1>
-        </div>
+      <div className="flex">
+        <h1>
+          Del Mastro & Gonzalez Gallery{" "}
+          <i
+            className="fas fa-shopping-cart"
+            id="cart"
+            onClick={() => {
+              checkOut(allPaint);
+            }}
+          ></i>
+        </h1>
+      </div>
 
-        <blockquote><i>"If you could say it in words, there would be no reason to paint." Edward Hopper</i></blockquote>
-        <div className="flexContainer">
-          <ul className= "paintingsGallery grid-container">
-            {paintings.map((painting) => {
-                return(
-                  <li key={painting.key}>
-                    <img src={painting.name.url} alt={painting.name.description}></img>
-                    <p className="space">{painting.name.title}</p>
-                    <p className="Pspace">{painting.name.description}</p>
-                    <p className="price">${painting.name.price} CAN</p>
-                     {painting.name.value === true
-                      ? <button onClick={ () => {addToCart(painting)
-                      }
-                    }>Buy now</button>
-                        : <button  className="space" disabled>Added to Cart</button>
-                    }
-                  </li>
-                )
+      <blockquote>
+        <i>
+          "If you could say it in words, there would be no reason to paint."
+          Edward Hopper
+        </i>
+      </blockquote>
+      <div className="flexContainer">
+        <ul className="paintingsGallery grid-container">
+          {paintings
+            .slice()
+            .reverse()
+            .map((painting) => {
+              return (
+                <li key={painting.key}>
+                  <img
+                    src={painting.name.url}
+                    alt={painting.name.description}
+                  ></img>
+                  <p className="space">{painting.name.title}</p>
+                  <p className="space">{painting.name.artist}</p>
+                  <p className="space">{painting.name.date}</p>
+                  <p className="Pspace">{painting.name.description}</p>
+                  <p className="price">${painting.name.price} CAN</p>
+                  {painting.name.value === true ? (
+                    <button
+                      onClick={() => {
+                        addToCart(painting);
+                      }}
+                    >
+                      Buy now
+                    </button>
+                  ) : (
+                    <button className="space" disabled>
+                      Added to Cart
+                    </button>
+                  )}
+                </li>
+              );
             })}
-          </ul>
+        </ul>
+      </div>
+      <div className="peterSection flexBox">
+        <div className="divStyle">
+          <h3>Upcoming exhibition</h3>
+          <img className="peterImg" src={imageSrc} alt={"peter paint"}></img>
         </div>
-         <div className="peterSection flexBox">
-                <div className="divStyle">
-                  <h3>Upcoming exhibition</h3>
-                  <img className="peterImg" src={imageSrc} alt={"peter paint"} ></img>
-                </div>
-                 <div>
-                      <h4>Peter Del Mastro</h4>
-                 </div>
-         </div>
-   </div>
+        <div>
+          <h4>Peter Del Mastro</h4>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/Popup.js
+++ src/Popup.js
@@ -1,24 +1,35 @@
 //function to display the checkout box
 const CheckOutPopup = function (props) {
-
-    return (
-        <div className="cartBox">
-            <h2>Items in your Cart:</h2>
-                {props.arrayOfPaintings.map((cartPaint) => {
-                    return (
-                        <>
-                        { cartPaint.value === false ?
-                        <li className="gridBox list">
-                            <img className="cartImage" src={cartPaint.url} alt={cartPaint.description}></img>
-                            <p className="Pspace price cartPrice">{cartPaint.price} <i onClick={ () => {props.remove(props.arrayOfPaintings, cartPaint)}} className="far fa-trash-alt"></i></p>
-                        </li>
-                        : null
-                        }
-                        </>
-                    )
-                })}
-                <p className='total price'>Total: ${props.totalCost} CAN</p>
-        </div>)
-    }
+  return (
+    <div className="cartBox">
+      <h2>Items in your Cart:</h2>
+      {props.arrayOfPaintings.map((cartPaint) => {
+        return (
+          <>
+            {cartPaint.value === false ? (
+              <li className="gridBox list">
+                <img
+                  className="cartImage"
+                  src={cartPaint.url}
+                  alt={cartPaint.description}
+                ></img>
+                <p className="Pspace price cartPrice">
+                  {cartPaint.price}{" "}
+                  <i
+                    onClick={() => {
+                      props.remove(props.arrayOfPaintings, cartPaint);
+                    }}
+                    className="far fa-trash-alt"
+                  ></i>
+                </p>
+              </li>
+            ) : null}
+          </>
+        );
+      })}
+      <p className="total price">Total: ${props.totalCost} CAN</p>
+    </div>
+  );
+};
 
 export default CheckOutPopup;
